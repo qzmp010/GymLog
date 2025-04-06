1eeba4c5f5e5691354e01a22be211d8ae7cf9ff0
qzmp010 <66999686+qzmp010@users.noreply.github.com>
2025-04-06 06:44:18 -0700

    add logout feature

--- app/src/main/java/com/scheng/gymlog/MainActivity.java
+++ app/src/main/java/com/scheng/gymlog/MainActivity.java
@@ -1,16 +1,26 @@
 package com.scheng.gymlog;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
 import com.scheng.gymlog.database.GymLogRepository;
 import com.scheng.gymlog.database.entities.GymLog;
+import com.scheng.gymlog.database.entities.User;
 import com.scheng.gymlog.databinding.ActivityMainBinding;
 import java.util.ArrayList;
 
@@ -27,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
   int reps = 0;
 
   //TODO: add login info
-  int loggedInUserId = -1;
+  private int loggedInUserId = -1;
+  private User user;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     setContentView(binding.getRoot());
 
     loginUser();
+    invalidateOptionsMenu();
 
     if (loggedInUserId == -1) {
       Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
@@ -58,7 +70,58 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void loginUser() {
+    user = new User("Drew", "password");
     loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.logout_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    MenuItem item =menu.findItem(R.id.logoutMenuItem);
+    item.setVisible(true);
+    item.setTitle(user.getUsername());
+    item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+      @Override
+      public boolean onMenuItemClick(@NonNull MenuItem item) {
+        showLogoutDialog();
+        return false;
+      }
+    });
+    return true;
+  }
+
+  private void showLogoutDialog() {
+    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(MainActivity.this);
+    final AlertDialog alertDialog = alertBuilder.create();
+
+    alertBuilder.setMessage("Logout?");
+
+    alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        logout();
+      }
+    });
+
+    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        alertDialog.dismiss();
+      }
+    });
+
+    alertBuilder.create().show();
+  }
+
+  private void logout() {
+    //todo: finish
+    startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
   }
 
   static Intent mainActivityIntentFactory(Context context, int userId) {
